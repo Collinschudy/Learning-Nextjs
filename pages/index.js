df882a7e585bfb02726786fdc291dacf4d779c68
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-05-08 15:41:53 +0100

    completed the Assets, Metadata, and CSS lesson

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I am a frontend web developer and this is my first next app.
+          You can contact me on {' '}<a href='https://twitter.com/dubem_ebuka'>twitter</a>
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
